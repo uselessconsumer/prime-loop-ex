dd08855af4534ef74aec7e66944603f603b36ad7
uselessconsumer <eliottdale@gmail.com>
2023-08-15 15:09:08 -0400

    create a function to determine if the number <= input is a prime number

--- script.js
+++ script.js
@@ -15,3 +15,26 @@
 //If isPrime = true log it in the console
 //if isPrime = false do not log
 //++isPrime, loop until isPrime = 'n'
+
+//input = the number to loop to
+//isPrime = any number starting at 2 that will be tested if it is prime
+//testPrime = every number between 1 and isPrime that will test if isPrime is a prime number
+
+let n = 10;
+let isPrime;
+// I might need to create a function?
+
+for (isPrime = 2; isPrime <= n; ++isPrime) {
+    
+    function isPrimeTest() {
+        let testPrime;
+        
+        for (testPrime = 2; testPrime < isPrime; ++testPrime) { //loop to see if isPrime is a prime number
+            if (testPrime % isPrime) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
